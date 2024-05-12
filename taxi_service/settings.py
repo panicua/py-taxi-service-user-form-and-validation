239095dd66bfadcfa2d1b5b8d2b26bc39b684e4e
kekW <panicua12@gmail.com>
2024-05-12 23:31:10 +0300

    requirements again.

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
-    "bootstrap5",
     "crispy_forms",
+    "crispy_bootstrap4",
     "taxi",
 ]
 
